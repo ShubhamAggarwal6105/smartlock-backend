b952488bdcde6906cd2b4d251400420cde9b1280
hetvi3012 <157871396+hetvi3012@users.noreply.github.com>
2024-06-08 14:50:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import userRouter from "./routes/user.js";
 import itemRouter from "./routes/inventory.js";
+import mongoose from "mongoose";
+import itemLogRoutes from "./routes/itemLog.js";
 import OTProuter from "./routes/otp.js";
 import {config} from "dotenv";
 
@@ -15,6 +17,18 @@ app.use(express.json());
 app.use("/users", userRouter);
 app.use("/items", itemRouter);
 app.use("/otp", OTProuter);
+app.use("/api/itemlog", itemLogRoutes);
+
+mongoose.connect('mongodb://localhost:27017/inventoryDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 
 app.get("/", (req, res)=>{
     res.send("Nice working");
